58fbfb9da6f8c5d6deecc6e091570b9673e5aff2
Nick Loman <n.j.loman@bham.ac.uk>
2016-06-29 16:19:17 +0000

    add institutions model

--- brynweb/userdb/models.py
+++ brynweb/userdb/models.py
@@ -4,10 +4,13 @@ from django.db.models import *
 from django.contrib.auth.models import User
 from phonenumber_field.modelfields import PhoneNumberField
 
+class Institution(Model):
+    name = CharField(max_length=100)
+
 class GroupProfile(Model):
     position = CharField(max_length=50)
     department = CharField(max_length=50)
-    institution = CharField(max_length=50)
+    institution = CharField(max_length=100)
     phone_number = PhoneNumberField(max_length=20)
     research_interests = CharField(max_length=500)
     intended_climb_use = CharField(max_length=500)
