026c26b4f7dbd09ac9575cb49014d305da863d64
Andy Smith <andysmith.d@gmail.com>
2016-06-29 17:21:23 +0100

    Change large CharFields to TextFields

--- brynweb/userdb/models.py
+++ brynweb/userdb/models.py
@@ -4,18 +4,17 @@ from django.db.models import *
 from django.contrib.auth.models import User
 from phonenumber_field.modelfields import PhoneNumberField
 
+
 class GroupProfile(Model):
     position = CharField(max_length=50)
     department = CharField(max_length=50)
     institution = CharField(max_length=50)
     phone_number = PhoneNumberField(max_length=20)
-    research_interests = CharField(max_length=500)
-    intended_climb_use = CharField(max_length=500)
-    held_mrc_grants = CharField(max_length=500)
+    research_interests = TextField()
+    intended_climb_use = TextField()
+    held_mrc_grants = TextField()
 
     def __unicode__(self):
         return self.user.get_full_name()
 
 User.group = property(lambda u: GroupProfile.objects.get_or_create(user=u)[0])
-
-
