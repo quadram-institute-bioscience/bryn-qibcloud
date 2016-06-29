3b0fa3dda4feffe9b55a1dcb69bb0f9a3e3311b5
Nick Loman <n.j.loman@bham.ac.uk>
2016-06-29 16:43:48 +0000

    nuke database, add messages to registration

--- brynweb/userdb/views.py
+++ brynweb/userdb/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
 from forms import CustomUserCreationForm, GroupProfileForm
+from django.contrib import messages
 
 def register(request):
     if request.method == 'POST':
@@ -11,7 +13,9 @@ def register(request):
             profileform.user = user
             profileform.save()
 
-            return HttpResponse('registered')
+            messages.success(request, 'Thank you for registering. Your request will be approved by an administrator and you will receive an email with further instructions')
+
+            return HttpResponseRedirect(reverse('home'))
     else:
         userform = CustomUserCreationForm()
         profileform = GroupProfileForm()
