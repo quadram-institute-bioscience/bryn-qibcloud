ff3eaa20b4a314a8ff1aea2ebdec570f2e149597
Andy Smith <andysmith.d@gmail.com>
2016-07-20 12:46:17 +0100

    Make user the default server type choice

--- brynweb/home/forms.py
+++ brynweb/home/forms.py
@@ -1,8 +1,14 @@
-
 from django import forms
 from django.forms.widgets import PasswordInput
 from django.core.validators import RegexValidator
 from userdb.models import Region
+
+
+SERVER_TYPE_CHOICES = [
+  ('user', 'User server'),
+  ('group', 'Group server'),
+]
+
 
 class LaunchServerForm(forms.Form):
     server_name = forms.CharField(label='Server name', max_length=50, required=True,
@@ -12,9 +18,10 @@ class LaunchServerForm(forms.Form):
                                      message='Only letters, numbers and hyphens in server name',
                                 )
                               ])
-    server_type = forms.ChoiceField(choices=(('group', 'Group server'), ('user', 'User server')))
+    server_type = forms.ChoiceField(choices=(SERVER_TYPE_CHOICES))
 
     password = forms.CharField(label='New server password', widget=PasswordInput, required=True)
+
 
 class LaunchImageServerForm(forms.Form):
     def __init__(self, images, keys, *args, **kwargs):
@@ -45,6 +52,7 @@ class LaunchImageServerForm(forms.Form):
     server_key_name_choice = forms.ChoiceField(required=True)
     server_key_name = forms.CharField(label='Server key name', help_text='A descriptive name for your key', required=False)
     server_key = forms.CharField(label='Server key', help_text='Your SSH public server key for access to the serer.', widget=forms.Textarea, required=False)
+
 
 class RegionSelectForm(forms.Form):
     region = forms.ModelChoiceField(queryset=Region.objects.filter(disabled=False))
